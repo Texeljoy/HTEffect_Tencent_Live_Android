b4e2fccf499d45343745a755bdf9fded6d760a46
deer <1770323038@qq.com>
2022-09-29 11:53:47 +0800

    增加子模块

--- App/src/main/java/com/tencent/mlvb/apiexample/MLVBApplication.java
+++ App/src/main/java/com/tencent/mlvb/apiexample/MLVBApplication.java
@@ -23,7 +23,7 @@ public class MLVBApplication extends Application {
         TXLiveBase.getInstance().setLicence(instance, GenerateTestUserSig.LICENSEURL, GenerateTestUserSig.LICENSEURLKEY);
 
         //todo --- hteffect start
-        HTEffect.shareInstance().initHTEffect( this, "67f72e4da36542379b2e22fed7e45439", new InitCallback() {
+        HTEffect.shareInstance().initHTEffect( this, "YOUR_APP_ID", new InitCallback() {
             @Override public void onInitSuccess() {
 
             }
